d83cf6e1b2c743192a58fea311f03ea0625e1aa1
Lukaslakas31 <lukejuniel@gmail.com>
2023-04-28 11:25:45 +0800

    added skills

--- flutter_act1/lib/main.dart
+++ flutter_act1/lib/main.dart
@@ -187,9 +187,57 @@ class SkillsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        // enter code here
-        );
+    return Scaffold(
+         body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Languages:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '- Dart',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- JavaScript',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Frameworks/Libraries:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '- Flutter',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- React',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Other Skills:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '- Git',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- Agile Methodologies',
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
